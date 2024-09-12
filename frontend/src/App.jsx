5203b5ae708cf90f3f26e588a1fc6f0e6dd444fa
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-09-12 05:49:08 +0530

    modified login-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 
-
-import WelcomePage from './pages/WelcomePage'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
 
 
   return (
-    <>
-      <WelcomePage />
-
-    </>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
